1ab3df0ad228f8cb7c648ab133d9a8bad7550f69
Nguyễn Anh Kiệt <kietnguyen0814@gmail.com>
2017-07-31 15:38:19 +0700

    Initial Commit

---
+++ AnhKiet_TimTich/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AnhKiet_TimTich
+//
+//  Created by Kiet Nguyen on 7/31/17.
+//  Copyright © 2017 Kiet Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
